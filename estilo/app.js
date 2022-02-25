b6d2d0dac08cf1841dc743fe0fd62dfccda6249c
Gustavo Ferreira dos Santos <gustavoferreira73@live.com>
2022-02-25 11:16:34 -0300

    Versão 1.0.0
    Atualizando informações

--- estilo/app.js
+++ estilo/app.js
@@ -1,8 +1,24 @@
-function aviso(){
-    alert('Indisponivel para dispositivos moveis!')
+
+const btnMobile = document.getElementById('btn-mobile');
+
+function toggleMenu(){
+    const nav = document.getElementById('nav');
+
+    nav.classList.toggle('active');
 }
 
-function fechar(){
-    alert('Ação não permitida no momento aguardando push!')
-    //gus1.style.display="none"
+btnMobile.addEventListener('click', toggleMenu);
+
+function aviso(){
+    alert('Servidor externo indisponível ');
+}
+function sobre(){
+    alert('Não é possível acessar a DASHBOARD do site Qlegis.')
+}
+function fecha(){
+    fer.style.display='none'
+}
+function falecom(){
+    alert('Você será direcionado para o WhastApp!')
+    location='https://api.whatsapp.com/send?phone=5561995084318&text=Tenho%20uma%20duvida%2C%20'
 }
