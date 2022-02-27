0c11cf9449279dba6c5e643d678b6eccfd8ef0cd
Gustavo Ferreira dos Santos <gustavoferreira73@live.com>
2022-02-27 18:19:19 -0300

    V1.0.0
    Implementando sistema de de navegação aprimorado

--- estilo/app.js
+++ estilo/app.js
@@ -1,16 +1,16 @@
-
 const btnMobile = document.getElementById('btn-mobile');
 
 function toggleMenu(){
     const nav = document.getElementById('nav');
-
-    nav.classList.toggle('active');
+    nav.classList.toggle('active')
 }
 
 btnMobile.addEventListener('click', toggleMenu);
 
+
+//Remover esta parte
 function aviso(){
-    alert('Servidor externo indisponível ');
+    alert('Pagina em manutenção!')
 }
 function sobre(){
     alert('Não é possível acessar a DASHBOARD do site Qlegis.')
@@ -22,3 +22,4 @@ function falecom(){
     alert('Você será direcionado para o WhastApp!')
     location='https://api.whatsapp.com/send?phone=5561995084318&text=Tenho%20uma%20duvida%2C%20'
 }
+//Remover esta parte
