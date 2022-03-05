450e62867a5b16f86ccb164f000620c0c34a7801
Gustavo Ferreira dos Santos <gustavoferreira73@live.com>
2022-03-04 22:25:43 -0300

    V1.0.0
    Aplicando novas paginas ao site

--- estilo/app.js
+++ estilo/app.js
@@ -8,6 +8,29 @@ function toggleMenu(){
 btnMobile.addEventListener('click', toggleMenu);
 
 
+/*====opções do perfil=======*/
+
+function opPerfil(){
+    opcUsuario.style.display='block'
+}
+
+function irParaPag(){
+    alert('Direcionar')
+}
+
+function sairOpUser(){
+    opcUsuario.style.display='none'
+}
+
+function deslogar(){
+    location='../planos/login.html'
+}
+
+function chamaOpUser(){
+    opcoesUsuario.style.display='block'
+}
+/*======opções do perfil=====*/
+
 //Remover esta parte
 function aviso(){
     alert('Pagina em manutenção!')
@@ -29,5 +52,10 @@ function pagat(){
 
 function cadastro(){
     alert('O banco de dados não foi estabelecido!')
+}
+
+function verifiarLog(){
+    alert("Você esta em uma verção de teste!")
+    location='../logado/logado.html'
 }
 //Remover esta parte
