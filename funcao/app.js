9b298974a936a38a828773939adfa0ffa131d68b
Gustavo Ferreira dos Santos <gustavoferreira73@live.com>
2022-03-31 11:07:40 -0300

    Atualização
    Paginas atualizadas e organizadas

--- funcao/app.js
+++ funcao/app.js
@@ -4,54 +4,50 @@ function toggleMenu(){
     const nav = document.getElementById('nav');
     nav.classList.toggle('active')
 }
-
 btnMobile.addEventListener('click', toggleMenu);
 
-/*====== Dados de cadastro =======*/
-function cadastro(){
-
-    var nome = document.getElementById('cNome').value
-    var sobreNome = document.getElementById('cSobreNome').value
-    var email = document.getElementById('cMail').value
-    var senhaPri = document.getElementById('cSenha').value
-    var senhaSeg = document.getElementById('mSenha').value
-    var tel = document.getElementById('cTel').value
-    var estado = document.getElementById('cEst').value
-
-    if(senhaPri != senhaSeg){
-        alert('Senhas não confere')
-    } else if (nome == '' || sobreNome == '' || email == '' || senhaPri == '' || senhaSeg == '' || tel == ''){
-        alert('Falta dados')
-    } else {
-        localStorage.nome = nome
-        localStorage.email = email
-        localStorage.senha = senhaPri
-
-        alert('Cadastrado com sucesso!') 
-    }
+function irSobre(){
+    location='sobre.html'
 }
-/*====== Dados de cadastro =======*/
 
-/*====== LOGIN ======*/
-function verifiarLog(){
-
-    var emailUser = document.getElementById('tMail').value
-    var senhaUser = document.getElementById('tSenha').value
-
-    var email = localStorage.email
-    var senha = localStorage.senha
-
-    if( email != emailUser){
-        alert('E-mail errado!')
-    } else if (senha != senhaUser) {
-        alert('Senha errado!')
-    } else{
-        location='../001_logado/inicioLogado.html'
-    }
-
+function irAjuda(){
+    location='ajuda.html'
 }
-/*====== LOGIN ======*/ 
+
+function irTermos(){
+    location='termos.html'
+}
+
+function irDuvidas(){
+    location='duvidas.html'
+}
+
+function irPlnaos(){
+    location='planos.html'
+}
+
+function irAjuda(){
+    location='ajuda.html'
+}
 
 function faleConosco(){
-    location='060_faleConosco_grupos_sobre/faleConosco.html'
+    location='faleConosco.html'
+}
+
+function btnEnvia(){
+    alert('Mensagem enviada')
+}
+
+function avisoDeLogin(){
+    avisoEntra.style.display='block'
+}
+
+function fechaAvisoDeLogin(){
+    avisoEntra.style.display='none'
+}
+
+
+
+function avisoLink(){
+    alert('Sem link')
 }
