284aa9a775a0d054edd9aaaa95c429379ba3f4f0
Gustavo Ferreira dos Santos <gustavoferreira73@live.com>
2022-04-03 14:44:46 -0300

    teste
    fazendo o teste mobile

---
+++ 01_logado/00-inicioLogado/funcao/app.js
@@ -0,0 +1,20 @@
+function temas(){
+    alert('Indisponível')
+}
+function ajuda(){
+    alert('Indisponível')
+}
+
+function fechaNotifi(){
+    notif.style.display='none'
+}
+function chamaNotifi(){
+    notif.style.display='block'
+}
+function chamaOpcUser(){
+    opcsUser.style.display='block'
+}
+function irLogin(){
+    alert('Sem link')
+    location='inicioLogado.html'
+}
