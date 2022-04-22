5dd3293d7c611e4120af4025ac4a19ac060fe66e
Gustavo Ferreira <gustavoferreira73@live.com>
2022-04-22 20:16:02 -0300

    Atualização

---
+++ Pages/Derpartamento.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc.Pages
+{
+    public class Derpartamento
+    {
+        public int Id { get; set; }
+    }
+}
